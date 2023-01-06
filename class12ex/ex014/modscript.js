d3ffc67ea4b4783127e95a384a6b762582acb2be
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-06 18:00:17 -0500

    dup

--- class12ex/ex014/modscript.js
+++ class12ex/ex014/modscript.js
@@ -1,1 +1,20 @@
-alert('Hello')
+function loading() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('image')
+    var data = new Date()
+    var hour = data.getHours()
+    msg.innerHTML = `It is ${hour} o'clock`
+    if (hour >= 0 && hour < 12) {
+        // Morning
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hour >= 12 && hour < 18) {
+        // Afternoon
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        // Evening
+        img.src = 'noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
