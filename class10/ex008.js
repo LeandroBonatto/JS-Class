d80100b0329e763831d7687a0cd40a87749c993a
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-06 10:59:35 -0500

    exercises

--- class10/ex008.js
+++ class10/ex008.js
@@ -1,4 +1,6 @@
-var speed = 60.5
+var speed = 62
 console.log(`Your speed is ${speed} kph`)
-console.log(`You are over limit`)
+if (vel > 60) {
+    console.log(`You are over limit. Penalty`)
+}
 console.log(`Fasten your seat belt`)
---
+++ class10/ex009.js
@@ -0,0 +1,7 @@
+var country = 'EUA'
+console.log(`Living em ${country}`)
+if(country == 'Brazil') {
+    console.log(`You are Brazilian`)
+} else {
+    console.log(`You are foreigner`)
+}
