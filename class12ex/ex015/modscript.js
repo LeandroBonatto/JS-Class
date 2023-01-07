db1c5386a8ee5509e0ea018cf681270daeec5d9e
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-07 00:22:14 -0500

    ex015

--- class12ex/ex015/modscript.js
+++ class12ex/ex015/modscript.js
@@ -1,1 +1,13 @@
-alert('Hello')
+function verify() {
+    var data = new Date()
+    var year = data.getFullYear()
+    var fyear = document.getElementById('txtyear')
+    var res = document.querySelector('div#res')
+    if (fyear.value.length == 0 || Number(fyear.value) > year) {
+        window.alert('[ERROR] Check fields and try again')
+    } else {
+        var fgender = document.getElementsByName('radgender')
+        var age = year - Number(fyear.value)
+        
+    }
+}
