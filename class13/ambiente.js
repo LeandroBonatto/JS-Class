74620f56d0ec46bd5cefbc93ae1586a60474d652
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-07 13:08:14 -0500

    Create ambiente.js

---
+++ class13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+while (c <= 6) {
+    console(`Passo ${c}`)
+    c++
+}
+
+
+
+/*
+console.log('How are you?')
+console.log('How are you?')
+console.log('How are you?')
+console.log('How are you?')
+*/
