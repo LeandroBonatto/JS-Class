9ea4ace185d4805279aa82f6ab92da2e14e59f89
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-07 17:46:09 -0500

    upd

--- class14ex/ex016/modscript.js
+++ class14ex/ex016/modscript.js
@@ -1,1 +1,12 @@
-alert('Hello')
+function count() {
+    let start = document.getElementById('txts')
+    let end = document.getElementById('txte')
+    let pace = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (start.value.length == 0 || end.value.length == 0 || pace.value.length == 0) {
+        window.alert('[ERROR] Missing field')
+    } else {
+        res.innerHTML = 'Counting: '
+    }
+}
