b4a912cddffe444e5a018fdaeb31007c338e47aa
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-07 00:29:25 -0500

    Update modscript.js

--- class12ex/ex015/modscript.js
+++ class12ex/ex015/modscript.js
@@ -8,6 +8,32 @@ function verify() {
     } else {
         var fgender = document.getElementsByName('radgender')
         var age = year - Number(fyear.value)
-        
+        var gender = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fgender[0].checked) {
+            gender = 'male'
+            if (age >= 0 && age < 10) {
+                // kid
+            } else if (age < 21) {
+                // young
+            } else if (age < 50) {
+                // adult
+            } else {
+                // old
+            }
+        }
+        if (fgender[0].checked) {
+            gender = 'male'
+            if (age >= 0 && age < 10) {
+                // kid
+            } else if (age < 21) {
+                // young
+            } else if (age < 50) {
+                // adult
+            } else {
+                // old
+            }
+        }
     }
 }
