530479c2bcddde03848ae78f0dd064a2be994ea2
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-07 19:04:42 -0500

    ex017

---
+++ class14ex/ex017/modscript.js
@@ -0,0 +1,12 @@
+function multiplicationTable() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Please, write a number')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10)
+        let item = document.createA
+    }
+}
