119ab2ad92cd0f2308fef3d0be33f4588b3874f1
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-08 14:47:55 -0500

    vector

--- class15/ambiente.js
+++ class15/ambiente.js
@@ -2,5 +2,5 @@ let num = [5, 8, 2, 9, 3]
 num.sort()
 num.push(1) // Firstly the program will sort(above) then it will push 1
 console.log(num)
-console.log(`The vetor has ${num.length} positions.`)
+console.log(`The vector has ${num.length} positions.`)
 console.log(`The first vetor value is ${num[0]}`)
---
+++ class15/vector.js
@@ -0,0 +1,14 @@
+let values = [8, 1, 7, 4, 2, 9]
+
+//console.log(values)
+
+/*
+console.log(values[0])
+console.log(values[1])
+console.log(values[2])
+.
+.
+.
+*/
+
+for (let pos=0; pos < values.length; pos++)
