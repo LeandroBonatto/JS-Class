6daa35e98ae24cae9a81ebbf7b553d5e3d5005ca
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-08 00:04:46 -0500

    upd017

--- class14ex/ex017/modscript.js
+++ class14ex/ex017/modscript.js
@@ -6,10 +6,13 @@ function multiplicationTable() {
     } else {
         let n = Number(num.value)
         let c = 1
-        while (c <= 10)
-        let item = document.createElement('option')
-        item.text = `${num} x ${c} = ${num*c}`
-        tab.appendChild(item)
-        c++
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
