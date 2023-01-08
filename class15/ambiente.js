7f6d94cfa8370c8c2b23bbada596536d11953c6a
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-08 15:11:37 -0500

    changes

--- class15/ambiente.js
+++ class15/ambiente.js
@@ -4,3 +4,5 @@ num.push(1) // Firstly the program will sort(above) then it will push 1
 console.log(num)
 console.log(`The vector has ${num.length} positions.`)
 console.log(`The first vetor value is ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`The 8 value is in the position ${pos}`)
---
+++ class15/vectorEx.js
@@ -0,0 +1,12 @@
+let values = [8, 1, 7, 4, 2, 9]
+
+
+/* for (let pos=0; pos < values.length; pos++) {
+    console.log(`The position ${pos} has the value ${values[pos]}`)
+}
+*/
+
+// Simplified
+for(let pos in num) {
+    console.log(`The position ${pos} has value ${values[pos]}`)
+}
