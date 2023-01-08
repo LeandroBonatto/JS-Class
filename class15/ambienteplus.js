56ec797b69f26aee109fa82a24d6c4ad3719a264
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-08 15:48:00 -0500

    upd

--- class15/ambienteplus.js
+++ class15/ambienteplus.js
@@ -1,10 +1,12 @@
 let num = [5, 8, 2, 9, 3]
+num.push(1)
 num.sort()
-num.push(1) // Firstly the program will sort(above) then it will push 1
 console.log(num)
 console.log(`The vector has ${num.length} positions.`)
 console.log(`The first vetor value is ${num[0]}`)
 let pos = num.indexOf(8)
-console.log(`The 8 value is in the position ${pos}`)
-
-
+if (pos == -1) {
+    console.log('The value was not found')
+} else {
+    console.log('The value is in the position ${pos}')
+}
